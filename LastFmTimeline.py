bdaf01dbfd66b34c5c5de4fc4548d7ce1fddc18f
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-08-12 20:50:46 -0700

    Update LastFmTimeline.py

--- LastFmTimeline.py
+++ LastFmTimeline.py
@@ -2,27 +2,49 @@
 from LastFmApi   import *
 from functions   import *
 from collections import OrderedDict
+from matplotlib  import pyplot
 
 
 LastFmGet = LastFmApi(
     key       = "REDACTED", # My key
     userAgent = "D3r3k523")
 
-
 username = get_username(LastFmGet)
-print()
-mode = get_mode()
-print()
-sort = get_sort()
-print()
+mode     = get_mode() # "Artist", "Album", "Track"
+sort     = get_sort() # "Scrobbles", "Rank"
 numItems = get_num_items(mode)
-print()
 
-charts = get_target_charts(LastFmGet, username)
-items  = get_items(LastFmGet, username, mode, numItems)
+items  = get_items(LastFmGet, username, mode, numItems) # List of strings
+charts = get_target_charts(LastFmGet, username)         # List of Weeks
 
 data = OrderedDict()
-init_data(charts, items, data)
+init_data(items, charts, data)
 load_data(LastFmGet, username, mode, sort, charts, data)
+# data = {
+#   item1: {
+#     chart1: entry,
+#     chart2: entry,
+#     ... },
+#   item2: {
+#     chart1: entry,
+#     chart2: entry,
+#     ... },
+#   ... }
 
-print_data(data)
+pyplot.title("{name}'s Top {numItems} {type}s Timeline".format(name=username, numItems=numItems, type=mode))
+pyplot.xlabel("Weeks")
+
+alphaMax = 0.6
+alphaMin = 0.1
+alphaInt = (alphaMax - alphaMin) / (numItems - 1)
+alpha = alphaMax
+
+for itemName, itemData in data.items():
+    pyplot.plot_date(itemData.values(),
+                     xdate = True,
+                     color=(0.75, 0, 0, alpha),
+                     label=itemName)
+    alpha -= alphaInt
+
+pyplot.legend()
+pyplot.show()
