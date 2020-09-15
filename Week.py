1b8df8c792e124e9a3263df67dcece8a4a1d16fa
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-09-14 20:45:36 -0700

    Update Week.py

--- Week.py
+++ Week.py
@@ -1,16 +1,16 @@
 
 from datetime import datetime
 
-
-# Stores a month and year
+# Stores start and end timestamps of a week
 class Week:
     def __init__(self, start, end):
         self.start = start
         self.end   = end
-        self.date  = datetime.fromtimestamp(end).strftime("%m/%d/%y")
+
+        self.date = datetime.fromtimestamp(self.end)
 
     def __str__(self):
-        return self.date
+        return self.date.strftime("%m/%d/%y")
 
     def __eq__(self, other):
         return isinstance(other, type(self)) and (self.start, self.end) == (other.start, other.end)
